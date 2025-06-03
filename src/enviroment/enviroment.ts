a86645328c4be8aca334d3d78d78219ba0eece87
baptist.onderdonck <baptist.onderdonck@alten.nl>
2025-06-03 12:14:22 +0200

    chore: Remove unused environment and configuration files for Elasticsearch and Kibana

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -3,12 +3,11 @@ export const environment = {
   apiKey: 'devKey',
   auth: {
     clientId: 'fab9c0df-a37b-4b41-864f-5fbe3b3b2f38',
-    serverUrl: 'http://localhost:9011',
-    redirectUri: 'http://localhost:4200/callback',
-    // postLogoutRedirectUri: 'http://localhost:4200/',
-    scope: 'openid email profile address phone offline_access claims',
+    serverUrl: 'http://fusionauth:9011',
+    redirectUri: 'http://fusionauth:4200/callback',
+    scope: 'openid email profile offline_access',
   },
   api: {
-    url: 'http://localhost:8090/api',
+    url: 'http://api:8090/api',
   },
 };
