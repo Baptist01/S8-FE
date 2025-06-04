24e2ef16fb69f05c87a78e9d963471ac9324872a
baptist.onderdonck <baptist.onderdonck@alten.nl>
2025-06-04 20:55:14 +0200

    fix: update server URL and redirect URI in environment configuration

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -3,8 +3,8 @@ export const environment = {
   apiKey: 'devKey',
   auth: {
     clientId: 'fab9c0df-a37b-4b41-864f-5fbe3b3b2f38',
-    serverUrl: 'https://tjeckposeidon136.com/',
-    redirectUri: 'https://tjeckposeidon136.com/callback',
+    serverUrl: 'https://auth.thecoderoomprivate.com/',
+    redirectUri: 'https://auth.thecoderoomprivate.com/callback',
     scope: 'openid email profile offline_access',
   },
   api: {
