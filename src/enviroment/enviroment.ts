5f7938c778595e6f289a47c139a63b137988d8ef
baptist.onderdonck <baptist.onderdonck@alten.nl>
2025-06-04 09:00:18 +0200

    fix: update server URLs to use localhost in environment configuration

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -3,11 +3,11 @@ export const environment = {
   apiKey: 'devKey',
   auth: {
     clientId: 'fab9c0df-a37b-4b41-864f-5fbe3b3b2f38',
-    serverUrl: 'http://fusionauth:9011',
-    redirectUri: 'http://fusionauth:4200/callback',
+    serverUrl: 'http://localhost:9011',
+    redirectUri: 'http://localhost:4200/callback',
     scope: 'openid email profile offline_access',
   },
   api: {
-    url: 'http://api:8090/api',
+    url: 'http://localhost:8090/api',
   },
 };
